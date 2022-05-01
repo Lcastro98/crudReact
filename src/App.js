3a90f6142deb2166b12662bf240f7dbc8799571e
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-30 23:06:04 -0500

    Tabla de usuarios
    Se crea un componente que contiene una tabla que organiza la información de los usuarios y muestra dos botones, una tabla con la informaciÂón de los usuarios y dos botones

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UserTable from './components/UserTable';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+
+  const usersData = [
+    { id: uuidv4(), name: 'Lorena', username: 'Anezoldyck' },
+    { id: uuidv4(), name: 'Felipe', username: 'Nirsch' },
+    { id: uuidv4(), name: 'Hernán', username: 'Crown' },
+  ]
+
+  const [users, setUser] = useState(usersData);
+
   return (
-    <div>Hola mundo</div>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users}/>
+        </div>
+      </div>
+    </div>
   );
 }
 
